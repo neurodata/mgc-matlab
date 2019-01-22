452c5b2cf8cd74830f95f07d72a12fa0e7bca88e
tpsatish95 <tpsatish95@gmail.com>
2019-01-22 13:28:27 -0500

    Fix Dcor and FastDcor

--- Code/Fast Code/FastTestDCor.m
+++ Code/Fast Code/FastTestDCor.m
@@ -6,6 +6,7 @@
 %% @param Y is an n*n distance matrix or a n*q data matrix;
 %% @param ns specifies the number of subsamples. Generally n/ns should be more than 4, and ns should be large than 10. 
 %% @param optionMethod is a string that specifies the method, supporting 'dcor' and 'hsic'.
+%% @param optionCenter is a string that specifies how the distance matrices are centered, including 'unbiased'(default) and 'biased'.
 %%
 %% @return P-value and test statistic.
 %%
@@ -14,7 +15,7 @@
 %% In comparison, FastTestMGC.m runs in O(n^2 log(n)+ns*n) and the original permutation test runs in O(r n^2 log(n)).
 %% This script is the fastest one, but has inferior power than the above two at the same sample size.
 %%
-function  [pval,stat]=FastTestDCor(X,Y,ns,optionMethod)
+function  [pval,stat]=FastTestDCor(X,Y,ns,optionMethod,optionCenter)
 % Example 1: ns=20, optionMethod='dcor'; %fastest dcor with power loss
 % Example 2: ns=20, optionMethod='hsic'; %fastest hsic with power loss
 
@@ -24,6 +25,10 @@ end
 if nargin<4
     optionMethod='dcor';  % use mgc by default
 end
+if nargin<5
+    optionCenter='unbiased';  % use unbiased by default
+end
+
 n=size(Y,1);
 S=floor(n/ns);
 % if full data size is no more than 4 times of nn, split to 4 samples --  too few 
@@ -40,7 +45,7 @@ for s=1:S
     sub=ns*(s-1)+1:ns*s;
     Xs=X(sub,:);
     Ys=YP(sub,:);
-    statA(s)=DCor(Xs,Ys,optionMethod);
+    statA(s)=DCor(Xs,Ys,optionMethod,optionCenter);
 end
 mu=0;
 sigma=std(statA)/sqrt(S);
