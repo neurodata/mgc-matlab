20c5ef66271e36771c71443c0d7824298534e441
cshen6 <11924683+cshen6@users.noreply.github.com>
2020-04-06 10:51:07 -0400

    update partial

--- Code/DCorPartialTest.m
+++ Code/DCorPartialTest.m
@@ -17,42 +17,40 @@
 %% @export
 
 function [corr,pval] = DCorPartialTest(X,Y,Z)
+
 n=size(X,1);
-eps=0.000001;
-
-[~,AC]=DCor(X,Z);
-[~,BC]=DCor(Y,Z);
-[~,CC]=DCor(Z,Z);
-
-[A]=DCorInput(X);
-[B]=DCorInput(Y);
-[C]=DCorInput(Z);
-[A,B]=DCorTransform(A,B,'unbiased',0);
-[C,~]=DCorTransform(C,A,'unbiased',0);
-
-PA=A-AC/CC*C;
-PB=B-BC/CC*C;
-cov=GlobalCov(PA,PB)/n/(n-3);
-varPA=GlobalCov(PA,PA)/n/(n-3);
-varPB=GlobalCov(PB,PB)/n/(n-3);
-corr=cov./real(sqrt(varPA*varPB)); % normalized
-% corr=corr/size(X,1)^2;% un-normalized
-
-if varPA<=eps || varPB<=eps
-    corr=0; % if either variance is non-positive, set corr to 0
+a=DCor(X,Y)-DCor(X,Z)*DCor(Z,Y);
+b=(1-DCor(X,Z)^2)^0.5*(1-DCor(Y,Z)^2)^0.5;
+if b==0
+    corr=0;
+else
+    corr=a/b;
 end
 pval=1-chi2cdf(corr*n+1,1); 
-%%% To check validity of chi-square test, verify the following (power should be no more than alpha):
-% n=100;p=1;power=0;rep=1000;alpha=0.05;
-% for i=1:rep
-% X=unifrnd(0,1,n,p);
-% Y=unifrnd(0,1,n,p);
-% Z=unifrnd(0,1,n,p);
-% [corr,pval] = DCorPartialTest(X,Y,Z);
-% if pval<alpha;
-% power=power+1/rep;
-% end
-% end
 
-function [cov]=GlobalCov(A,B)
-cov=sum(sum(A.*B));
+% eps=0.000001;
+% 
+% [~,AC]=DCor(X,Z);
+% [~,BC]=DCor(Y,Z);
+% [~,CC]=DCor(Z,Z);
+% 
+% [A]=DCorInput(X);
+% [B]=DCorInput(Y);
+% [C]=DCorInput(Z);
+% [A,B]=DCorTransform(A,B,'unbiased',0);
+% [C,~]=DCorTransform(C,A,'unbiased',0);
+% 
+% PA=A-AC/CC*C;
+% PB=B-BC/CC*C;
+% cov=GlobalCov(PA,PB)/n/(n-3);
+% varPA=GlobalCov(PA,PA)/n/(n-3);
+% varPB=GlobalCov(PB,PB)/n/(n-3);
+% corr=cov./real(sqrt(varPA*varPB)); % normalized
+% 
+% if varPA<=eps || varPB<=eps
+%     corr=0; % if either variance is non-positive, set corr to 0
+% end
+% pval=1-chi2cdf(corr*n+1,1); 
+% 
+% function [cov]=GlobalCov(A,B)
+% cov=sum(sum(A.*B));
--- Code/MGCPartialTest.m
+++ Code/MGCPartialTest.m
@@ -15,50 +15,34 @@
 %% @export
 
 function [corr,pval] = MGCPartialTest(X,Y,Z)
+
 n=size(X,1);
-eps=0.000001;
-
-[~,AC]=DCor(X,Z);
-[~,BC]=DCor(Y,Z);
-[~,CC]=DCor(Z,Z);
-
-[A]=DCorInput(X);
-[B]=DCorInput(Y);
-[C]=DCorInput(Z);
-[A,B]=DCorTransform(A,B,'unbiased',0);
-[C,~]=DCorTransform(C,A,'unbiased',0);
-
-PA=A-AC/CC*C;
-PB=B-BC/CC*C;
-localCor=MGCLocalCor(PA,PB);
-
-% [m,n]=size(localCor);
-corr=max(max(localCor));
-%[corr,~]=MGCSmoothing(localCor,m,n); 
-% if m==1 || n==1
-%     corr=localCor(end);
-%     optimalScale=m*n;
-%     R=1;
-% else
-%     % find the maximal within the significant region, return optimal scale and a connectec region of local correlations R
-%     [corr,optimalScale, R]=MGCSmoothing(localCor,m,n); 
-%varPA=MGCLocalCorr(PA,PA)/n/(n-3);
-%varPB=MGCLocalCorr(PB,PB)/n/(n-3);
-%corr=cov./real(sqrt(varPA*varPB)); % normalized
-% corr=corr/size(X,1)^2;% un-normalized
-
-% if varPA<=eps || varPB<=eps
-%     corr=0; % if either variance is non-positive, set corr to 0
-% end
+a=MGC(X,Y)-MGC(X,Z)*MGC(Z,Y);
+b=(1-MGC(X,Z)^2)^0.5*(1-MGC(Y,Z)^2)^0.5;
+if b==0
+    corr=0;
+else
+    corr=a/b;
+end
 pval=1-chi2cdf(corr*n+1,1); 
-%%% To check validity of chi-square test, verify the following (power should be no more than alpha):
-% n=100;p=1;power=0;rep=1000;alpha=0.05;
-% for i=1:rep
-% X=unifrnd(0,1,n,p);
-% Y=unifrnd(0,1,n,p);
-% Z=unifrnd(0,1,n,p);
-% [corr,pval] = DCorPartialTest(X,Y,Z);
-% if pval<alpha;
-% power=power+1/rep;
-% end
-% end
+
+% n=size(X,1);
+% 
+% [~,AC]=DCor(X,Z);
+% [~,BC]=DCor(Y,Z);
+% [~,CC]=DCor(Z,Z);
+% 
+% [A]=DCorInput(X);
+% [B]=DCorInput(Y);
+% [C]=DCorInput(Z);
+% [A,B]=DCorTransform(A,B,'unbiased',0);
+% [C,~]=DCorTransform(C,A,'unbiased',0);
+% 
+% PA=A-AC/CC*C;
+% PB=B-BC/CC*C;
+% localCor=MGCLocalCor(PA,PB);
+% 
+% corr=max(max(localCor));
+% %[corr,~]=MGCSmoothing(localCor,m,n); 
+% pval=1-chi2cdf(corr*n+1,1); 
+
